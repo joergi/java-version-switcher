5d3dae8456d8b88203b55c652f2e2672b49759bd
Jörg Liedl <j.liedl@dialogworks.de>
2016-08-23 14:50:15 +0200

    changes

--- j6.sh
+++ j6.sh
@@ -1,12 +1,11 @@
-sudo update-alternatives --install "/usr/bin/java" "java" "/home/joergi/dev/java/jdk1.6.0_45/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/home/joergi/dev/java/jdk1.6.0_45/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/home/joergi/dev/java/jdk1.6.0_45/bin/javaws" 1
-sudo update-alternatives --install "/usr/bin/jar" "jar" "/home/joergi/dev/java/jdk1.6.0_45/bin/jar" 1 
+jdk6="/home/joergi/dev/java/jdk1.6.0_45"
 
+sudo update-alternatives --set "java" $jdk6"/bin/java"
+sudo update-alternatives --set "javac" $jdk6"/bin/javac"
+sudo update-alternatives --set "javaws" $jdk6"bin/javaws"
+sudo update-alternatives --set "jar" $jdk6"/bin/jar" 
 
-sudo update-alternatives --set "java" "/home/joergi/dev/java/jdk1.6.0_45/bin/java"
-sudo update-alternatives --set "javac" "/home/joergi/dev/java/jdk1.6.0_45/bin/javac"
-sudo update-alternatives --set "javaws" "/home/joergi/dev/java/jdk1.6.0_45/bin/javaws"
-sudo update-alternatives --set "jar" "/home/joergi/dev/java/jdk1.6.0_45/bin/jar"
-
+echo "switched to "
 java -version
+
+ 
--- j8.sh
+++ j8.sh
@@ -1,12 +1,10 @@
-sudo update-alternatives --install "/usr/bin/java" "java" "/home/joergi/dev/java/jdk1.8.0_77/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/home/joergi/dev/java/jdk1.8.0_77/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/home/joergi/dev/java/jdk1.8.0_77/bin/javaws" 1
-sudo update-alternatives --install "/usr/bin/jar" "jar" "/home/joergi/dev/java/jdk1.8.0_77/bin/jar" 1 
+jdk8="/home/joergi/dev/java/jdk1.8.0_77"
 
+sudo update-alternatives --set "java" $jdk8"/bin/java"
+sudo update-alternatives --set "javac" $jdk8"/bin/javac"
+sudo update-alternatives --set "javaws" $jdk8/"bin/javaws"
+sudo update-alternatives --set "jar" $jdk8"/bin/jar" 
 
-sudo update-alternatives --set "java" "/home/joergi/dev/java/jdk1.8.0_77/bin/java"
-sudo update-alternatives --set "javac" "/home/joergi/dev/java/jdk1.8.0_77/bin/javac"
-sudo update-alternatives --set "javaws" "/home/joergi/dev/java/jdk1.8.0_77/bin/javaws"
-sudo update-alternatives --set "jar" "/home/joergi/dev/java/jdk1.8.0_77/bin/jar"
-
+echo "switched to "
 java -version
+ 
