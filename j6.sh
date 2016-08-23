c660e2ca5723d2c33c8cd65cf3d7c075deca307a
Joergi <joerg.liedl@rocket-design.de>
2016-08-23 16:11:22 +0200

    Update j6.sh
    bugfix

--- j6.sh
+++ j6.sh
@@ -2,7 +2,7 @@ jdk6="/home/joergi/dev/java/jdk1.6.0_45"
 
 sudo update-alternatives --set "java" $jdk6"/bin/java"
 sudo update-alternatives --set "javac" $jdk6"/bin/javac"
-sudo update-alternatives --set "javaws" $jdk6"bin/javaws"
+sudo update-alternatives --set "javaws" $jdk6"/bin/javaws"
 sudo update-alternatives --set "jar" $jdk6"/bin/jar" 
 
 echo "switched to "
