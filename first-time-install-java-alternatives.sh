0ceb6fd122ce6f54700c3045fdf0a2b334d41d08
Joergi <joerg.liedl@rocket-design.de>
2016-08-23 14:51:42 +0200

    Update first-time-install-java-alternatives.sh
    bugfix, adds $ before jdk

--- first-time-install-java-alternatives.sh
+++ first-time-install-java-alternatives.sh
@@ -9,14 +9,14 @@ sudo update-alternatives --install "/usr/bin/javaws" "javaws" $jdk6"/bin/javaws"
 sudo update-alternatives --install "/usr/bin/jar" "jar" $jdk6"/bin/jar" 1 
 
 # install java 7 as java alternative
-sudo update-alternatives --install "/usr/bin/java" "java" jdk7"/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" jdk7"/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" jdk7"/bin/javaws" 1
-sudo update-alternatives --install "/usr/bin/jar" "jar" jdk7"/bin/jar" 1 
+sudo update-alternatives --install "/usr/bin/java" "java" $jdk7"/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" $jdk7"/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" $jdk7"/bin/javaws" 1
+sudo update-alternatives --install "/usr/bin/jar" "jar" $jdk7"/bin/jar" 1 
 
 # install java 8 as java alternative
-sudo update-alternatives --install "/usr/bin/java" "java" jdk8"/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" jdk8"/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" jdk8"/bin/javaws" 1
-sudo update-alternatives --install "/usr/bin/jar" "jar" jdk8"/bin/jar" 1 
+sudo update-alternatives --install "/usr/bin/java" "java" $jdk8"/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" $jdk8"/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" $jdk8"/bin/javaws" 1
+sudo update-alternatives --install "/usr/bin/jar" "jar" $jdk8"/bin/jar" 1 
 
