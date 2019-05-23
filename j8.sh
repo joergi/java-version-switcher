7a5184e0f85631c4b25208765a7a3399c070bdc9
Joerg Liedl <joerg.liedl@flightright.de>
2019-05-23 11:22:08 +0200

    adds jdk 11 switcher

--- j8.sh
+++ j8.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 jdk8="/usr/lib/jvm/java-8-openjdk-amd64"
 
-sudo update-alternatives --install "/usr/bin/java" "java" $jdk8"/bin/java" 3
-sudo update-alternatives --install "/usr/bin/javac" "javac" $jdk8"/bin/javac" 3
-#sudo update-alternatives --install "/usr/bin/javaws" "javaws" $jdk8"/bin/javaws" 3
-sudo update-alternatives --install "/usr/bin/jar" "jar" $jdk8"/bin/jar" 3
+sudo update-alternatives --install "/usr/bin/java" "java" $jdk8"/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" $jdk8"/bin/javac" 1
+#sudo update-alternatives --install "/usr/bin/javaws" "javaws" $jdk8"/bin/javaws" 1
+sudo update-alternatives --install "/usr/bin/jar" "jar" $jdk8"/bin/jar" 1
 
 sudo update-alternatives --set "java" $jdk8"/bin/java"
 sudo update-alternatives --set "javac" $jdk8"/bin/javac"
